496aaf6f378918d0260dc99b6c578be990a58c68
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2023-09-24 18:39:13 +0900

    Add c-1

---
+++ customer_c-1.py
@@ -0,0 +1,21 @@
+# C-1.フルネームを取得できる
+
+# customerクラスの定義
+class Customer:
+
+    # 初期設定
+    def __init__(self, first_name, family_name):
+        self.first_name = first_name
+        self.family_name = family_name
+
+    # full＿nameの定義
+    def full_name(self):
+        return self.first_name + " " + self.family_name
+
+# kenの場合
+ken = Customer(first_name="Ken", family_name="Tanaka")
+print(ken.full_name())
+
+# tomの場合
+tom = Customer(first_name="Tom", family_name="Ford")
+print(tom.full_name())
